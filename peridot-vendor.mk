cd9f533d88a301d4ea805eefdee13f2f91b25af9
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-26 15:30:54 +0530

    peridot: build audio blobs from source
    Change-Id: Iad69a9868f80ffc2ca4290e10d1c7f8a73d282fb

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1220,9 +1220,6 @@ PRODUCT_PACKAGES += \
     libTxPwrJni \
     libVoiceSdk \
     libWlanServiceJni \
-    lib_bt_aptx \
-    lib_bt_ble \
-    lib_bt_bundle \
     lib_bt_lhdc \
     lib_misound_asc \
     libadm \
